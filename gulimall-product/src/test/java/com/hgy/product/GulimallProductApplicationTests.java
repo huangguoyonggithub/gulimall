6c178da01656d34bbe90ac992b77572a24ab59c4
huangguoyong <2847082948@qq.com>
2024-03-30 12:54:12 +0800

    项目初始结构创建

--- gulimall-product/src/test/java/com/hgy/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/hgy/product/GulimallProductApplicationTests.java
@@ -1,13 +1,28 @@
 package com.hgy.product;
 
+import com.hgy.product.entity.BrandEntity;
+import com.hgy.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
+
+    @Autowired
+    BrandService brandService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setName("华为");
+
+        brandService.save(brandEntity);
+
+
+        System.out.println("保存成功。。。");
     }
 
 }
